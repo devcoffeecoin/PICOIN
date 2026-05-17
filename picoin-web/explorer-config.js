957c1719d1c2174cfc73674252c1a660001e4eae
Johann Sining <titoplomo@gmail.com>
2026-05-17 10:16:52 -0500

    fix explorer publit tesnet

--- picoin-web/explorer-config.js
+++ picoin-web/explorer-config.js
@@ -1,8 +1,8 @@
 window.PICOIN_EXPLORER_CONFIG = {
-  apiBaseUrl: "http://198.211.100.89:8000",
-  refreshMs: 30000,
+  apiBaseUrl: "https://api.picoin.science",
+  refreshMs: 10000,
   nodes: [
-    { label: "nyc1 bootstrap", url: "http://198.211.100.89:8000" },
-    { label: "nyc3 validator", url: "http://165.227.181.138:8000" }
+    { label: "bootstrap", url: "https://api.picoin.science" },
+    { label: "validator", url: "https://validator.picoin.science" }
   ]
 };
