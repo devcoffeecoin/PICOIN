60ff3ac5238ed434995c73296d5c5612fb2af958
Johann Sining <titoplomo@gmail.com>
2026-06-03 15:19:45 -0500

    Route explorer API directly to bootstrap

--- api/validator-proxy.js
+++
@@ -1,8 +0,0 @@
-const { proxyHandler } = require("./_proxy");
-
-module.exports = async function handler(req, res) {
-  return proxyHandler(req, res, {
-    name: "Picoin validator API",
-    target: process.env.PICOIN_VALIDATOR_API_URL || "https://api.picoin.science",
-  });
-};
--- picoin-web/api/bootstrap-proxy.js
+++
@@ -1,8 +0,0 @@
-const { proxyHandler } = require("./_proxy");
-
-module.exports = async function handler(req, res) {
-  return proxyHandler(req, res, {
-    name: "Picoin bootstrap API",
-    target: process.env.PICOIN_BOOTSTRAP_API_URL || "https://api.picoin.science",
-  });
-};
